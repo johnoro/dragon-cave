dcb23323167250c65792397de38124ea720259f6
johnoro <officialjohnorourke@gmail.com>
2022-07-06 17:42:23 -0400

    Adds todo note regarding accounts

--- be/src/main/java/com/johnoro/DragonCave/controller/AccountController.java
+++ be/src/main/java/com/johnoro/DragonCave/controller/AccountController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+// TODO: add authorization & authentication
 @CrossOrigin(origins = { "http://localhost:3000" })
 @RestController
 @RequestMapping("/api/account")
