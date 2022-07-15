cf9149955a8df036275968e108655cc2fa2d3e8a
johnoro <officialjohnorourke@gmail.com>
2022-07-15 10:33:57 -0400

    Fixes state hook mistake

--- fe/src/components/game/intro/Cave.js
+++ fe/src/components/game/intro/Cave.js
@@ -7,7 +7,8 @@ const Cave = (props) => {
         "It is dark and spooky...",
         "A large dragon jumps out in front of you! He opens his jaws and..."
     ];
-    const [dragon, setDragon] = useState({});
+    const [dragonName, setDragonName] = useState('');
+    const [dragonType, setDragonType] = useState('');
 
     useEffect(() => {
         // perhaps only a call to DragonService here is needed?
@@ -32,7 +33,7 @@ const Cave = (props) => {
             ))}
             <p>
                 {/* To later check dragon type */}
-                {dragon
+                {dragonName
                     ? "Gobbles you down in one bite!"
                     : "Spits out a horde of treasure at your feet!"
                 }
